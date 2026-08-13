dcac11020d13f22e1191a788cf65ae4100b89fed
t89082549405-sudo <t89082549405@gmail.com>
2026-08-13 21:36:16 +0500

    Update xam_net.cc

--- src/xenia/kernel/xam/xam_net.cc
+++ src/xenia/kernel/xam/xam_net.cc
@@ -970,7 +970,8 @@ dword_result_t NetDll_XNetInAddrToXnAddr_entry(dword_t caller, dword_t in_addr,
     std::memcpy(xn_addr->abEnet, mac.raw(), MacAddress::MacAddressSize);
 
     if (xid_ptr != nullptr) {
-      const uint64_t session_id = kernel_state()->GetXboxLiveAPI()->GetSystemlinkID();
+      const uint64_t session_id = 
+          kernel_state()->GetXboxLiveAPI()->GetSystemlinkID();
 
       XNKID* sessionId_ptr = kernel_memory()->TranslateVirtual<XNKID*>(xid_ptr);
 
